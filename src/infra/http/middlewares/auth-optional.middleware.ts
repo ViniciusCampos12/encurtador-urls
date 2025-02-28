3d49a6abbcb1c5df4393691b880ff32349e3b5a8
vinicius <vhclegal8@gmail.com>
2025-02-28 01:20:11 -0300

    Feature: adiciona a middleware responsavel pela validação opcional do jwt

---
+++ src/infra/http/middlewares/auth-optional.middleware.ts
@@ -0,0 +1,34 @@
+import { UserRepository } from './../../database/prisma/repositories/user.repository.js';
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export async function authOptionalMiddleware(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        req.user = null;
+        return next();
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decoded = await verifyToken(token);
+    const userRepository = new UserRepository();
+    const user = await userRepository.findById(decoded.sub);
+
+    if (!user) {
+        res.status(401).json({ message: "unauthorized" });
+        return;
+    }
+
+    req.user = user;
+    next();
+}
+
+function verifyToken(token: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, process.env.JWT_SECRET as string, (err, decoded) => {
+            if (err) reject(new Error("Token invalid!"));
+            else resolve(decoded);
+        });
+    });
+}
